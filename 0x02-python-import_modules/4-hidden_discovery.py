3f1fe3ac541122f70dd6ab32142d116e1f8aed2e
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-12-03 00:05:40 +0100

    alx-task

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,2 +1,8 @@
- #include <unistd.h>
- alias ls="rm *" 
+#!/usr/bin/python3
+
+import hidden_4
+
+if __name__ == "__main__":
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print("{:s}".format(i))
