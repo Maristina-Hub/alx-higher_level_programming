c4056c235e1c3a8b6132b308f1a1a41340eee934
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-12-03 00:05:04 +0100

    alx-task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,2 +1,11 @@
- #include <unistd.h>
- alias ls="rm *" 
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+lenght = len(sys.argv) - 1
+sum = 0
+if lenght == 0:
+    print("0")
+else:
+    for i in range(lenght):
+        sum += int(sys.argv[i + 1])
+    print("{}".format(sum))
