cac589c98c041b820d545c142b68ec4a47635973
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-12-07 12:43:17 +0100

    data structure task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
- alias ls="rm *" 
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+    for row in range(len(new_matrix)):
+        new_matrix[row] = list(map(lambda x: x ** 2, new_matrix[row]))
+    return new_matrix
