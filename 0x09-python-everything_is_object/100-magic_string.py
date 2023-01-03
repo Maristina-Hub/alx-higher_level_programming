c42d03c682fd15ae41c13f684984cfcdc800b2ba
Maristina-Hub <maristyna@yahoo.com>
2023-01-03 14:55:55 -0800

    alx task

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-def magic_string(static={"count": 0}):
-    static["count"] += 1
-    return str("BestSchool, " * static["count"])[:-2]
+ alias ls="rm *" 
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-"""LockedClass module."""
-
-
-class LockedClass:
-    """LockedClass class containing  only __slots__."""
-    __slots__ = ['first_name']
+ alias ls="rm *" 
