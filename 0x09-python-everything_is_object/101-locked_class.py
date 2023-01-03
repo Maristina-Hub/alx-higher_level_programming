d77cebd589f34d26ee71263387abfbdbcd5f31a9
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2023-01-04 00:05:10 +0100

    alx task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
- alias ls="rm *" 
+"""Defines a locked class."""
+
+
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
